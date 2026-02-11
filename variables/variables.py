4b9eddd67aa9cea046eab5c44bd786cba0c91208
Adryano Junior <jusamsung10@gmail.com>
2026-02-11 12:49:59 -0300

    adding variables to a Python file

---
+++ variables/variables.py
@@ -0,0 +1,18 @@
+LOGIN_DATA = {
+    "correctData": {
+        "username": "admintest@gmail.com",
+        "password": "Test123$"
+    },
+    "wrongData": {
+        "username": "testuser123",
+        "password": "testuser"
+    }
+}
+
+ERROR_MESSAGES = {
+    "noCredentials": "Erro: Nome de usuário é obrigatório.",
+    "noUsername": "Erro: Nome de usuário é obrigatório.",
+    "noPassword": "Erro: O campo da senha está vazio.",
+    "wrongCredentials": "Erro: O usuário testuser123 não está registrado neste site. Se você não está certo de seu nome de usuário, experimente o endereço de e-mail.",
+    "emailInvalid": "Erro: O usuário testuser123 não está registrado neste site. Se você não está certo de seu nome de usuário, experimente o endereço de e-mail."
+}
